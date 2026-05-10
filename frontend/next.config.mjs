c82d8d1608f68cd0cf9435700f3197da851b8ce7
codeorbit-tech <codeorbit.technologies2026@gmail.com>
2026-05-10 15:15:46 +0530

    Deploy: Ignore Node.js modules in client build to fix exceljs error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   devIndicators: {
     buildActivity: false,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+        child_process: false,
+        readline: false,
+        rimraf: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
